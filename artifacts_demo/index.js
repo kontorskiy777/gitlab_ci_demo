d4b8402bb8f7786eeeb8a5e8324f27dedc44e293
kontorskiy777 <kontorskiy777@gmail.com>
2022-05-24 13:02:59 +0300

    Initial Commit

---
+++ artifacts_demo/index.js
@@ -0,0 +1,12 @@
+//need to install express module
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+  res.send('This is my first Node application!')
+})
+
+app.listen(port, () => {
+  console.log(`Application is listening at http://localhost:${port}`)
+})
